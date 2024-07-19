3560e3eb042a2a4888ce0a7aa5b92222564b113e
rizkiihza <rizki.ihza@gmail.com>
2024-07-19 21:11:13 +0700

    [feature] implement slow ping for demo

--- simple_apis/main.go
+++ simple_apis/main.go
@@ -6,11 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 var LOGGER = log.New(os.Stdout, "INFO:", log.Ldate)
 
 func ping(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("Pong\n"))
+}
+
+func slowPing(w http.ResponseWriter, req *http.Request) {
+	time.Sleep(5 * time.Second)
 	w.WriteHeader(200)
 	w.Write([]byte("Pong\n"))
 }
@@ -28,6 +35,7 @@ func main() {
 		port = "9000"
 	}
 	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/slow-ping", slowPing)
 	http.HandleFunc("/call", call)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
